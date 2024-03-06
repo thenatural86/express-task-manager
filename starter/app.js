ef9f3330e725919d9ae9cb475762fd8faeca9bae
thenatural86 <philip.smalls86@gmail.com>
2024-03-06 16:19:34 -0300

    starter routes

--- starter/app.js
+++ starter/app.js
@@ -1,3 +1,10 @@
-console.log('Task Manager App')
-jkshfdgjh
-s
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Hello World')
+})
+
+const port = 3000
+app.listen(port, console.log(`Server is listening on port ${port}...`))
