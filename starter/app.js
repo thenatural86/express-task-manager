e39d9402f6dfc7ac116c0ee51d16b88406ce2602
thenatural86 <philip.smalls86@gmail.com>
2024-03-06 18:20:29 -0300

    setup routes and controller folders

--- starter/app.js
+++ starter/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
 
 // routes
 app.get('/hello', (req, res) => {
   res.send('Hello World')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 app.listen(port, console.log(`Server is listening on port ${port}...`))
