831d1e129fa539bf74fc860acf9f41944726cee2
thenatural86 <philip.smalls86@gmail.com>
2024-03-12 14:49:26 -0300

    try catch block in controller

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
-// const connectionString =
-
 const connectDB = (url) => {
   return mongoose.connect(url)
 }
 
 module.exports = connectDB
-
-// .then(() => console.log('connected to the DB...'))
-// .catch((error) => console.log(error))
