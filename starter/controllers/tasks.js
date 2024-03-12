3a18cb70311246c7ae0e519f5f76e0c5fa317919
thenatural86 <philip.smalls86@gmail.com>
2024-03-12 16:04:51 -0300

    update route

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -29,8 +29,23 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+
   // res.json({ id: req.params.id })
 }
 const deleteTask = async (req, res) => {
